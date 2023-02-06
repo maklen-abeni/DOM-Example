788a0b21f830b1ce4eb458420423dd484b2b7d2c
maklen <abenezertabu@gmail.com>
2023-02-06 11:39:30 +0300

    filter items

--- domcrashcourse/dom.js
+++ domcrashcourse/dom.js
@@ -28,18 +28,18 @@ if(e.target.classList.contains('delete')){
     }
 }
 }
-//  function filterItems (e){
-//     var text = e.target.value.toLowerCase();
-//     var items = itemList.getElementsByTagName('li');
-//     Array.from(items).forEach(function (item){
-//         var itemName = item.firstChild.textContent;
-//         if(itemName.toLowerCase().indexOf(text) != -1){
-//          item.style.display = 'block';  
-//         } 
-//         else {
-//             item.style.display = 'none';
+ function filterItems (e){
+    var text = e.target.value.toLowerCase();
+    var items = itemList.getElementsByTagName('li');
+    Array.from(items).forEach(function (item){
+        var itemName = item.firstChild.textContent;
+        if(itemName.toLowerCase().indexOf(text) != -1){
+         item.style.display = 'block';  
+        } 
+        else {
+            item.style.display = 'none';
 
-//         }
-//     })
+        }
+    })
 
-//  }
+ }
