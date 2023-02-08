b915f387fced9a896191e9ca56c73fa7f10bf95d
maklen <abenezertabu@gmail.com>
2023-02-08 12:22:43 +0300

    added js file

--- domcrashcourse/dom.js
+++ domcrashcourse/dom.js
@@ -1,45 +1,52 @@
-var form = document.getElementById('addForm');
-var itemList = document.getElementById('items');
-var filter = document.getElementById('filter');
+// var form = document.getElementById('addForm');
+// var itemList = document.getElementById('items');
+// var filter = document.getElementById('filter');
 
-form.addEventListener('submit', addItem);
-itemList.addEventListener('click', removeItem);
-filter.addEventListener('keyup', filterItems);
+// form.addEventListener('submit', addItem);
+// itemList.addEventListener('click', removeItem);
+// filter.addEventListener('keyup', filterItems);
 
-function addItem(e){
-    e.preventDefault()
-    var newItem = document.getElementById('item').value;
-    var li = document.createElement('li');
-    li.className = 'list-group-item';
+// function addItem(e){
+//     e.preventDefault()
+//     var newItem = document.getElementById('item').value;
+//     var li = document.createElement('li');
+//     li.className = 'list-group-item';
     
-    li.appendChild(document.createTextNode(newItem));
-    var deleteBtn = document.createElement('button');
-    deleteBtn.className = 'btn btn-danger btn-sm float-right delete';
-    deleteBtn.appendChild(document.createTextNode('x'));
-    li.appendChild(deleteBtn);
-    itemList.appendChild(li);
-}
+//     li.appendChild(document.createTextNode(newItem));
+//     var deleteBtn = document.createElement('button');
+//     deleteBtn.className = 'btn btn-danger btn-sm float-right delete';
+//     deleteBtn.appendChild(document.createTextNode('x'));
+//     li.appendChild(deleteBtn);
+//     itemList.appendChild(li);
+// }
 
-function removeItem (e){
-if(e.target.classList.contains('delete')){
-    if(confirm('Are you Sure?')){
-        var li = e.target.parentElement;
-        itemList.removeChild(li);
-    }
-}
-}
- function filterItems (e){
-    var text = e.target.value.toLowerCase();
-    var items = itemList.getElementsByTagName('li');
-    Array.from(items).forEach(function (item){
-        var itemName = item.firstChild.textContent;
-        if(itemName.toLowerCase().indexOf(text) != -1){
-         item.style.display = 'block';  
-        } 
-        else {
-            item.style.display = 'none';
+// function removeItem (e){
+// if(e.target.classList.contains('delete')){
+//     if(confirm('Are you Sure?')){
+//         var li = e.target.parentElement;
+//         itemList.removeChild(li);
+//     }
+// }
+// }
+//  function filterItems (e){
+//     var text = e.target.value.toLowerCase();
+//     var items = itemList.getElementsByTagName('li');
+//     Array.from(items).forEach(function (item){
+//         var itemName = item.firstChild.textContent;
+//         if(itemName.toLowerCase().indexOf(text) != -1){
+//          item.style.display = 'block';  
+//         } 
+//         else {
+//             item.style.display = 'none';
 
-        }
-    })
+//         }
+//     })
 
- }
+//  }
+let re = null
+for(let i = 0; i<10; i ++){
+    re = /cat/g;
+    re.test = 'catastrophy';
+    
+}
+console.log(re);
